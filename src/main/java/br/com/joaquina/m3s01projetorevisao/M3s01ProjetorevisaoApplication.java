ee102a71906349ef4113c73cb21fb09020231ad0
kahsh-augustin <gabriel.augustin@edu.sc.senai.br>
2023-05-03 19:58:42 -0300

    primeiro commit

---
+++ src/main/java/br/com/joaquina/m3s01projetorevisao/M3s01ProjetorevisaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaquina.m3s01projetorevisao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M3s01ProjetorevisaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M3s01ProjetorevisaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/joaquina/m3s01projetorevisao/M3s01ProjetorevisaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaquina.m3s01projetorevisao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M3s01ProjetorevisaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
